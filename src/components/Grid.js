0cee6f2161a1756f9d0a2be1a9b92982dacd11c1
Dan Spector <danno2423@gmail.com>
2020-07-09 17:08:40 -0400

    Update Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Grid.module.scss';
+import styles from "./Grid.module.scss";
 
-const Grid = () => (
-  <div className={styles.grid}>
-    <div className={styles.square1}>One</div>
-    <div className={styles.square2}>Two</div>
-    <div className={styles.square3}>Three</div>
-    <div className={styles.square4}>Four</div>
-  </div>
-)
+function Grid() {
+  return (
+    <div className={styles.grid}>
+      <div className={styles.square1}>One</div>
+      <div className={styles.square2}>Two</div>
+      <div className={styles.square3}>Three</div>
+      <div className={styles.square4}>Four</div>
+    </div>
+  );
+}
 
 export default Grid;
